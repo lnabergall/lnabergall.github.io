81f8eb3162ac29c79366302374048c490b350036
Kel Chan <kel@kelvinchan.ca>
2020-08-07 18:35:48 -0400

    changed the update script to escape html quote symbols.

--- update.py
+++ update.py
@@ -24,9 +24,10 @@ TEMPLATE = """
         </div>
 """
 
+html_escape = lambda s: s.replace("'","&#39;").replace("\"","&quot;")
 google_form = [l.decode('utf-8') for l in urlopen(URL).readlines()]
 output = "".join([MARKER_BEGIN] + [
-    TEMPLATE % (t,i+1,fact,author)
+    TEMPLATE % (t,i+1,html_escape(fact),author)
     for i,(t,author,fact) in enumerate(csv.reader(google_form[1:]))
 ] + [MARKER_END])
 
