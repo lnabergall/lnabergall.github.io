0a5a626a2bdc5d9bbd98faa5bf42d7d783569be5
Kel Chan <kel@kelvinchan.ca>
2020-08-10 00:39:58 -0400

    “I don't mind making jokes, but I don't want to look like one.” ― Marilyn Monroe Also added a major security flaw to update script because software need bugs.
    1. Uses bootstrap 4 now.
    2. Doesn't look like ants made the website on mobiles.
    3. Some a shady fix to a python SSL problem. I suspect this is only local to my
    computer. But hey, why not introduce a bug for everyone during COVID times. In
    all honesty, I think this is not an issue.

--- update.py
+++ update.py
@@ -1,16 +1,13 @@
 #!/usr/local/bin/python3
-import csv
-from urllib.request import urlopen
 
-# keep the spaces here cuz it looks nicer in the HTML
-# also keep the last empty line.
+# Keep the spaces here cuz it looks nicer in the HTML.
 URL = r"https://docs.google.com/spreadsheets/d/1gug657PzPp4B8tfHHsZ9BEcAFihkFIBDBHtJ-O_pWdQ/export?format=csv"
 MARKER_BEGIN = "        <!--MARKER_BEGIN-->\n"
 MARKER_END = "        <!--MARKER_END-->\n"
 TEMPLATE = """
         <!-- %s -->
         <div class="row fact">
-          <div class="col s8 push-s2">
+          <div class="col-sm-12 col-md-8 offset-md-2">
             <div class="quote">
               <span class="label">%d. </span>
               <span>
@@ -24,13 +21,30 @@ TEMPLATE = """
         </div>
 """
 
+# -------
+# WARNING
+# -------
+#
+# After Kel's python upgrade to 3.7.3, the script now gives me some kinda of SSL
+# validation error because of the https in the link. Chaning it to http doesn't
+# help because Google redirects it to https.
+#
+# In true algebraic combinators style, we simply asks python to not validate SSL
+# certificate for the https link. # What can possibly go wrong?
+#
+from urllib.request import urlopen
+from ssl import SSLContext
+google_form = [l.decode('utf-8', 'replace') for l in urlopen(URL,context=SSLContext()).readlines()]
+
+# Read and turn stuff from CSV to arrays.
+import csv
 html_escape = lambda s: s.replace("'","&#39;").replace("\"","&quot;")
-google_form = [l.decode('utf-8', 'replace') for l in urlopen(URL).readlines()]
 output = "".join([MARKER_BEGIN] + [
     TEMPLATE % (t,i+1,html_escape(fact),author)
     for i,(t,author,fact) in enumerate(csv.reader(google_form[1:]))
 ] + [MARKER_END])
 
+# Use the above template to turn them into HTML.
 html = ""
 with open('index.html', 'r') as f:
     skip = False
